d3c6e27d2c0263a1a257bc71c993a9af655937eb
Michael Friese <michael@frie.se>
2013-05-26 13:54:37 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# TS3Soundboard Music Bot Webinterface
+
+This project is just a simple demo of the new remote-control feature of my TS3-Soundboard Plugin that can be used to queue music.
+
+I will update this from time to time but feel free to fork and send pull requests!
---
+++ server.js
@@ -0,0 +1,93 @@
+var
+	express = require('express'),
+	dgram = require('dgram'),
+	fs = require('fs');
+
+var BASE_PATH = '/home/bot/music/';
+
+var playlist = [];
+var tracks = [];
+var isPlaying = 0;
+var currentlyPlaying = -1;
+
+// Prepare webserver
+var app = express();
+app.listen(8080);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/api/tracks', function (req, res) {
+	res.setHeader('Content-Type', 'application/x-json');
+	var result = [];
+	tracks.forEach(function (track, id) {
+		result.push({
+			id: id,
+			name: track.name
+		});
+	});
+	res.end(JSON.stringify(result));
+});
+
+app.get('/api/playing', function (req, res) {
+	res.setHeader('Content-Type', 'application/x-json');
+	if (isPlaying) {
+		res.end(JSON.stringify({ id: currentlyPlaying, name: tracks[currentlyPlaying].name }));
+	} else {
+		res.end(JSON.stringify(false));
+	}
+});
+
+app.get('/api/playlist', function (req, res) {
+	res.setHeader('Content-Type', 'application/x-json');
+	res.end(JSON.stringify(playlist));
+});
+
+app.post('/api/play/:id', function (req, res) {
+	console.log('Playing: ' + req.params.id);
+	playById(req.params.id);
+	res.end();
+});
+
+// Prepare UDP socket
+var cmdSocket = dgram.createSocket('udp4');
+var recSocket = dgram.createSocket('udp4');
+
+recSocket.bind(19112);
+recSocket.on('message', function (msg) {
+	console.log('Got notification from plugin.');
+	isPlaying = false;
+	if (playlist.length > 0) {
+		var track = playlist.shift();
+		playById(track.id);
+		currentlyPlaying = track.id;
+	}
+});
+
+// Get all files in that directory
+fs.readdir(BASE_PATH, function (err, files) {
+	if (err) {
+		throw new Error('Could not find files in basePath. Aborting.');
+	}
+	files.forEach(function (file) {
+		tracks.push({ file: BASE_PATH + file, name: file });
+	});
+});
+
+// Helper functions
+var playById = function (id) {
+	if (tracks.length <= id) {
+		console.log('Invalid track.');
+		return;
+	}
+	var track = tracks[id];
+	if (isPlaying) {
+		playlist.push({
+			id: id,
+			name: track.name
+		})
+	} else {
+		isPlaying = true;
+		currentlyPlaying = id;
+		var cmd = new Buffer("/music " + track.file);
+		cmdSocket.send(cmd, 0, cmd.length, 19111, '127.0.0.1');
+	}
+};
